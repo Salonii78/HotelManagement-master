e48d2887e8d0af5992b3dcfaf12111ce2b696d47
Salonii78 <salonishah797@gmail.com>
2024-09-08 23:58:29 +0530

    initial commit

---
+++ application/logs/log-2024-09-08.php
@@ -0,0 +1,23 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2024-09-08 20:19:03 --> Severity: Notice  --> Only variable references should be returned by reference C:\xampp\htdocs\hotelmanagement-master\system\core\Common.php 257
+ERROR - 2024-09-08 20:19:03 --> Severity: 8192  --> Creation of dynamic property CI_URI::$config is deprecated C:\xampp\htdocs\hotelmanagement-master\system\core\URI.php 72
+ERROR - 2024-09-08 20:19:03 --> Severity: 8192  --> Creation of dynamic property CI_Router::$uri is deprecated C:\xampp\htdocs\hotelmanagement-master\system\core\Router.php 89
+ERROR - 2024-09-08 20:19:03 --> Severity: 8192  --> Creation of dynamic property CI_Input::$security is deprecated C:\xampp\htdocs\hotelmanagement-master\system\core\Input.php 93
+ERROR - 2024-09-08 20:19:03 --> Severity: 8192  --> Creation of dynamic property CI_Input::$uni is deprecated C:\xampp\htdocs\hotelmanagement-master\system\core\Input.php 99
+ERROR - 2024-09-08 20:19:03 --> Severity: 8192  --> Creation of dynamic property Welcome::$exceptions is deprecated C:\xampp\htdocs\hotelmanagement-master\system\core\Controller.php 46
+ERROR - 2024-09-08 20:19:03 --> Severity: 8192  --> Creation of dynamic property Welcome::$log is deprecated C:\xampp\htdocs\hotelmanagement-master\system\core\Controller.php 46
+ERROR - 2024-09-08 20:19:03 --> Severity: 8192  --> Creation of dynamic property Welcome::$benchmark is deprecated C:\xampp\htdocs\hotelmanagement-master\system\core\Controller.php 46
+ERROR - 2024-09-08 20:19:03 --> Severity: 8192  --> Creation of dynamic property Welcome::$hooks is deprecated C:\xampp\htdocs\hotelmanagement-master\system\core\Controller.php 46
+ERROR - 2024-09-08 20:19:03 --> Severity: 8192  --> Creation of dynamic property Welcome::$config is deprecated C:\xampp\htdocs\hotelmanagement-master\system\core\Controller.php 46
+ERROR - 2024-09-08 20:19:03 --> Severity: 8192  --> Creation of dynamic property Welcome::$utf8 is deprecated C:\xampp\htdocs\hotelmanagement-master\system\core\Controller.php 46
+ERROR - 2024-09-08 20:19:03 --> Severity: 8192  --> Creation of dynamic property Welcome::$uri is deprecated C:\xampp\htdocs\hotelmanagement-master\system\core\Controller.php 46
+ERROR - 2024-09-08 20:19:03 --> Severity: 8192  --> Creation of dynamic property Welcome::$router is deprecated C:\xampp\htdocs\hotelmanagement-master\system\core\Controller.php 46
+ERROR - 2024-09-08 20:19:03 --> Severity: 8192  --> Creation of dynamic property Welcome::$output is deprecated C:\xampp\htdocs\hotelmanagement-master\system\core\Controller.php 46
+ERROR - 2024-09-08 20:19:03 --> Severity: 8192  --> Creation of dynamic property Welcome::$security is deprecated C:\xampp\htdocs\hotelmanagement-master\system\core\Controller.php 46
+ERROR - 2024-09-08 20:19:03 --> Severity: 8192  --> Creation of dynamic property Welcome::$input is deprecated C:\xampp\htdocs\hotelmanagement-master\system\core\Controller.php 46
+ERROR - 2024-09-08 20:19:03 --> Severity: 8192  --> Creation of dynamic property Welcome::$lang is deprecated C:\xampp\htdocs\hotelmanagement-master\system\core\Controller.php 46
+ERROR - 2024-09-08 20:19:03 --> Severity: 8192  --> Creation of dynamic property Welcome::$load is deprecated C:\xampp\htdocs\hotelmanagement-master\system\core\Controller.php 49
+ERROR - 2024-09-08 20:19:03 --> Severity: 8192  --> Optional parameter $type declared before required parameter $list is implicitly treated as a required parameter C:\xampp\htdocs\hotelmanagement-master\system\helpers\html_helper.php 106
+ERROR - 2024-09-08 20:19:03 --> Severity: 8192  --> Creation of dynamic property Welcome::$db is deprecated C:\xampp\htdocs\hotelmanagement-master\system\core\Loader.php 343
+ERROR - 2024-09-08 20:19:04 --> Severity: 8192  --> Creation of dynamic property CI_DB_mysqli_driver::$stricton is deprecated C:\xampp\htdocs\hotelmanagement-master\system\database\DB_driver.php 87
